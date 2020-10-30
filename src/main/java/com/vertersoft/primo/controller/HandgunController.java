12411eb9e4f720900d31290a7346c5bd0125e61f
VerterDesp <allaknar@gmail.com>
2020-10-30 22:10:04 +0200

    added exeptions

---
+++ src/main/java/com/vertersoft/primo/controller/HandgunController.java
@@ -0,0 +1,35 @@
+package com.vertersoft.primo.controller;
+
+import com.vertersoft.primo.model.guns.handgun.Handgun;
+import com.vertersoft.primo.repository.HandgunRepository;
+import com.vertersoft.primo.service.HandgunService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("handgun")
+public class HandgunController {
+
+    private final HandgunService handgunService;
+
+    @Autowired
+    public HandgunController(HandgunService handgunService) {
+        this.handgunService = handgunService;
+    }
+
+    @GetMapping
+    public List<Handgun> findAllHandguns() {
+        return handgunService.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Handgun findOne(@PathVariable Long id) {
+        return handgunService.findOne(id);
+    }
+
+}
---
+++ src/main/java/com/vertersoft/primo/service/HandgunService.java
@@ -0,0 +1,30 @@
+package com.vertersoft.primo.service;
+
+import com.vertersoft.primo.exeptions.NotFoundException;
+import com.vertersoft.primo.model.guns.handgun.Handgun;
+import com.vertersoft.primo.repository.HandgunRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HandgunService {
+
+    private HandgunRepository handgunRepository;
+
+    @Autowired
+    public HandgunService(HandgunRepository handgunRepository) {
+        this.handgunRepository = handgunRepository;
+    }
+
+    public List<Handgun> findAll() {
+        return handgunRepository.findAll();
+    }
+
+    public Handgun findOne(Long id) {
+        return handgunRepository
+                .findById(id)
+                .orElseThrow(NotFoundException::new);
+    }
+}
