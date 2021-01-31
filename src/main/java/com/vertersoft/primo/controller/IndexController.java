76ddcfc2582fa38f4d3112a4abeb0562a85679d3
VerterDesp <allaknar@gmail.com>
2021-01-31 03:56:21 +0200

    frontend module added

--- src/main/java/com/vertersoft/primo/controller/IndexController.java
+++ src/main/java/com/vertersoft/primo/controller/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping
-    public String index() {
-        return "index";
-    }
+//    @GetMapping
+//    public String index() {
+//        return "index";
+//    }
 }
